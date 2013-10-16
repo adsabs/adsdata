7c28eab7180373e771d862928764e0d9fe32b461
Jay Luker <jluker@cfa.harvard.edu>
2013-10-16 13:58:18 -0400

    * use lockfile to prevent clobbering of running proc * fixing script progress output messages

--- scripts/sync_build.sh
+++ scripts/sync_build.sh
@@ -4,14 +4,33 @@ SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 BASEPATH=`dirname $SCRIPTPATH`
 
+# activate our virtualenv
+. `dirname $BASEPATH`/python/bin/activate
+
+FROM_MONGO="mongodb://adszee:27017/solr4ads"
+LOCKFILE="$SCRIPTPATH/.build_sync.lock"
+
+if [ -f $LOCKFILE ]
+then
+    echo "Lock file exists. Aborting."
+    exit 1
+else
+    echo "Creating lockfile"
+    touch $LOCKFILE
+fi
+
 echo "#############" `date` ": synching data sources #################"
 python $BASEPATH/scripts/sync_mongo_data.py "$@" sync
 
-echo "#############" `date` ": synching data files ###################"
-python $BASEPATH/scripts/copy_fulltext.py --from_mongo=mongodb://adszee:27017/solr4ads "$@"
+echo "#############" `date` ": copying fulltext ###################"
+python $BASEPATH/scripts/copy_fulltext.py --from_mongo=$FROM_MONGO "$@"
 
-echo "#############" `date` ": synching data files ###################"
+echo "#############" `date` ": building docs collection ###################"
 python $BASEPATH/scripts/build_docs.py "$@" build
 
-echo "#############" `date` ": synching data files ###################"
+echo "#############" `date` ": processing deletions ###################"
 python $BASEPATH/scripts/deletions.py "$@" delete
+
+echo "Removing lockfile"
+rm -f $LOCKFILE
+
